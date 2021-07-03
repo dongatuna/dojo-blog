708e477fb2ea5a6bd03ebc94041de2870569c5c9
dongatuna <ngatuna05@gmail.com>
2021-07-03 13:31:22 -0700

    add error handling

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
    
     //props are use to pass data from a parent component into a child component
  
@@ -10,8 +10,7 @@ const BlogList = ({ blogs, title, handleDelete }) => {
                 blogs.map((blog) => (
                         <div className="blog-preview" key={blog.id}>
                             <h2>{blog.title}</h2>
-                            <p>Written by: { blog.author } </p>
-                            <button onClick = { () => handleDelete(blog.id)}>delete blog</button>
+                            <p>Written by: { blog.author } </p>                            
                         </div>
                     )                     
                 )
--- src/Home.js
+++ src/Home.js
@@ -3,32 +3,45 @@ import BlogList from './BlogList'
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+    const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
 
-    const [name, setName] = useState('mario')
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id )
-        setBlogs(newBlogs)
-    }
     //use effect runs on every render
     useEffect(() => {
-        console.log(`use effect ran`)
+        setTimeout(() => {
+            fetch(`http://localhost:9000/blogs`)
+            .then(res => {
+                if(!res.ok){
+                    throw Error('could not fetch data for that resource')
+                }
+                return res.json()
+            })
+            .then(data => {
+                setBlogs(data)
+                setIsPending(false)
+                setError(null)
+            })
+            .catch(err => {
+                console.log(err.message)
+                setError(err.message)
+                setIsPending(false)
+            })
+        }, 1000);
+        
     //    console.log(`blogs ${blogs}`)
-    }, [name]) //empty array depencies ensures use effect only runs after the first render
+    }, []) //empty array depencies ensures use effect only runs after the first render
   
     
 
     return ( 
         <div className="home">
-            <BlogList blogs = { blogs } title = 'All blogs!' handleDelete = {handleDelete} />
-            {/* <BlogList blogs = { blogs.filter( blog => blog.author === 'mario')} title = "Mario's blogs!" /> */}
-            <button onClick = {() => setName('luigi')}>change name</button>
-            <p>{name}</p>
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div>}
+            { blogs && <BlogList blogs = { blogs } title = 'All blogs!'  /> }
+       
+            {/* <button onClick = {() => setName('luigi')}>change name</button> */}
+            {/* <p>{name}</p> */}
         </div>
      );
 }
